46d390a526dbfc2018485d71a60ecad3d0f981af
Lulu Cookies <eloise.chown@students.plymouth.ac.uk>
2023-07-20 16:12:12 +0100

    Setting up front-end and back-end connection

---
+++ Messaging System/1004-SPA-Project/1004-SPA-Project/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1004_SPA_Project
+{
+    public class HomeController : Controller
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class YourControllerNameController : ControllerBase
+        {
+            [HttpGet("data")]
+            public ActionResult<IEnumerable<string>> GetData()
+            {
+                // Replace this with your actual data retrieval logic
+                var data = new List<string> { "Data 1", "Data 2", "Data 3" };
+                return Ok(data);
+            }
+        }
+    }
+}
---
+++ Messaging System/1004-SPA-Project/1004-SPA-Project/wwwroot/js/main.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // page navigation and dynamic content loading
+    fetch('/api/datga')
+        .then(response => response.json())
+        .then(data => {
+            // Update the content of the #app element with the fetched data
+            const appElement = document.getElementById('app');
+            appElement.innerHTML = data.join('<br>');
+            console.log("Fetch success.")
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
